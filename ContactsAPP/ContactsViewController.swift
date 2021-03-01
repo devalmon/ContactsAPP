2debc725323c6ea65eda7c25c3abde9aa9e9f064
alex <onemandev@aol.com>
2021-03-01 18:18:56 +0300

    add contacts

--- ContactsAPP/ContactsViewController.swift
+++ ContactsAPP/ContactsViewController.swift
@@ -10,11 +10,14 @@ import UIKit
 class ContactsViewController: UIViewController {
 
     var searchBar = UISearchBar()
+    var tableView = UITableView()
+    var contacts = ["Анна","Маша","Михаил","Григорий","Оксана","Анна","Маша","Михаил","Григорий","Оксана","Анна","Маша","Михаил","Григорий","Оксана","Анна","Маша","Михаил","Григорий","Оксана","Маша","Михаил","Григорий","Оксана","Анна","Маша","Михаил","Григорий","Оксана","Анна","Маша","Михаил","Григорий","Оксана"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureVC()
         configureTextField()
+        configureTableView()
     }
     
     //MARK: - config
@@ -28,11 +31,46 @@ class ContactsViewController: UIViewController {
         view.addSubview(searchBar)
         searchBar.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            searchBar.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10),
+            searchBar.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
             searchBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 7),
-            searchBar.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
-            searchBar.heightAnchor.constraint(equalToConstant: 30)
+            searchBar.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            searchBar.heightAnchor.constraint(equalToConstant: 40)
         ])
     }
+    
+    private func configureTableView() {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.backgroundColor = UIColor.white
+        
+        NSLayoutConstraint.activate([
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.topAnchor.constraint(equalTo: searchBar.bottomAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+        
+    }
+}
+
+extension ContactsViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contacts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+        cell.textLabel?.text = contacts[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        return UIView()
+    }
+    
     
 }
