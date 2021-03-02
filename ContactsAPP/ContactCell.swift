9c782c245ed383f785435aee67aec78e8387794d
Alexey B <phlood@yandex.ru>
2021-03-03 01:28:42 +0300

    disable landscape

---
+++ ContactsAPP/ContactCell.swift
@@ -0,0 +1,23 @@
+//
+//  ContactCell.swift
+//  ContactsAPP
+//
+//  Created by Alexey Baryshnikov on 03.03.2021.
+//
+
+import UIKit
+
+class ContactCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
